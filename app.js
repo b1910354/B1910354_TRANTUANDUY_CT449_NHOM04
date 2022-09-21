3d5df768185c3f8a6eaba5bf736cdacdfbba04ec
KeyOne <trantuanduy.05112001@gmail.com>
2022-09-21 09:11:32 +0700

    setting routers

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 
 // Morgan will log response codes after the response is sent
 const morgan = require("morgan");
-// const contactsRouter = require("./app/routes/contact.route");
+const contactsRouter = require("./app/routes/contact.route");
 // const ApiError = require("./app/api-error");
 
 const app = express();
@@ -15,21 +15,21 @@ app.use(express.json());
 app.use(morgan('dev'));
 
 // register routers
-// app.use("/api/contacts", contactsRouter);
+app.use("/api/contacts", contactsRouter);
 
 app.get('/', (req, res, next) => {
     res.json({message: "Welcome to contact book application."});
 });
 
-// app.use((req, res, next) => {
-//     return next(new ApiError(404, "Resource not found"));
-// });
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
 
-// app.use((err, req, res, next) => {
-//     return res.status(err.statusCode || 500).json({
-//         message: err.message || "Internal Server Error",
-//     });
-// });
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
+});
 
 module.exports = app;
 
